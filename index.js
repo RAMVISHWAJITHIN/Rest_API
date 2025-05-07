9905cd167bd98ebdef733199bb5347cf6866c510
Ramvishwajithin Porandla <ramvishwajithin@gmail.com>
2025-05-07 22:33:00 +0530

    2nd  commit

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+require("dotenv").config(); // <--- load env variables
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Product = require("./models/product.model");
+
 const productRoute = require("./routes/product.route");
+
 async function main() {
-  await mongoose.connect(
-    "mongodb+srv://usernameoffline404:8DkGcayeHEmIjn30@cluster0.jtzbnlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  await mongoose.connect(process.env.MONGO_URL);
 }
+
 main()
   .then(() => {
-    console.log("connectd to db");
+    console.log("connected to db");
     app.listen(3000, () => {
       console.log("server running on 3000");
     });
   })
   .catch((err) => console.log(err));
 
-//middleware
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//routes
+// routes
 app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
